7de847a02023824f87646cb4eb36ee1c071e7e35
Emin GENC <emingenc@novit.ai>
2021-04-07 11:50:48 +0100

    FIX: camere.py __main__ edited. filename param added

--- camera.py
+++ camera.py
@@ -1,11 +1,19 @@
 from time import sleep
 from picamera import PiCamera
+import argparse
 
-if __name__ == "__main__":
+def run_camera(filename:str)-> None:
     camera = PiCamera()
     camera.start_preview()
     sleep(2)
-    for filename in camera.capture_continuous('img.jpg'):
+    for filename in camera.capture_continuous(f'{filename}.jpg'):
         print('Captured %s' % filename)
         sleep(2) # wait 2 sec
-        
+
+
+if __name__ == "__main__":
+    # python3 camera.py --name <filename>  
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name',default="img", type=str,  help='image file name')
+    opt = parser.parse_args()
+    run_camera(opt.name)
