1ab8acdf6c12e454194f6900a292d70d17e2575e
Emin GENC <emingenc@novit.ai>
2021-04-07 10:21:37 +0100

    Revert "ADD: Setup readme added. camera.py created"
    This reverts commit 349875c2939379e308e95549dceb133c076fcd12.

--- camera.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-from picamera import PiCamera
-
-camera = PiCamera()
-camera.start_preview()
-sleep(2)
-for filename in camera.capture_continuous('img.jpg'):
-    print('Captured %s' % filename)
-    sleep(2) # wait 2 sec
