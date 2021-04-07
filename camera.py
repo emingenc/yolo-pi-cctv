5b7c0da88545bc2ac4192e82447409cfd6234db3
Emin GENC <emingenc@novit.ai>
2021-04-07 10:40:08 +0100

    FIX: change requests are done. img.jpg left for sample

--- camera.py
+++ camera.py
@@ -1,9 +1,11 @@
 from time import sleep
 from picamera import PiCamera
 
-camera = PiCamera()
-camera.start_preview()
-sleep(2)
-for filename in camera.capture_continuous('img.jpg'):
-    print('Captured %s' % filename)
-    sleep(2) # wait 2 sec
+if __name__ == "__main__":
+    camera = PiCamera()
+    camera.start_preview()
+    sleep(2)
+    for filename in camera.capture_continuous('img.jpg'):
+        print('Captured %s' % filename)
+        sleep(2) # wait 2 sec
+        
