30ac62088e5723b331383e20c6a63e553137db50
Emin GENC <emingenc@novit.ai>
2021-04-08 16:35:14 +0100

    FIX: camera.py was closing when pi cant connect to server. Try except added and post method tried from pi to server

--- camera.py
+++ camera.py
@@ -5,24 +5,27 @@ import requests
 import socket
 
 
-def post_image(image_name:str,  image:list, url:str)-> None:
+def post_image(image_name:str,  image:str, url:str)-> None:
     device_name = socket.gethostname()
-    data = {
-                'device_name': device_name,
-                'image_name': image_name,
-                'image': image,     
-                                            }
 
-    post_data = requests.post(url = url, data = data)
+    data = f'"device_name": "{device_name}", "image_name": "{image_name}", "image": "{image}"'
+    data = '{'+data+'}'
 
+    response = requests.post(url,  data=data)
+    return response
 
 def run_camera(filename:str)-> None:
     camera = PiCamera()
     camera.rotation=270
     camera.start_preview()
     sleep(2)
+    count = 0
     for filename in camera.capture_continuous(f'{filename}.jpg'):
-        #post_image(image=img,url='http://192.168.88.132:8000/images')
+        count += 1 
+        try:
+            post_image(image_name=filename,image=count,url='http://192.168.88.132:8000/images/')
+        except socket.error as error:
+            print('Server connection error ')
         sleep(2) # wait 2 sec
 
 
