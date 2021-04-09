569ce4850a552b1649b8efa552c0b3e41d8e3002
Emin GENC <emingenc@novit.ai>
2021-04-09 09:25:40 +0100

    ADD: post2server is done frame number added to post data

--- camera.py
+++ camera.py
@@ -1,11 +1,20 @@
 from time import sleep
 from picamera import PiCamera
 import argparse
+import json
 import requests
 import socket
 import base64
 from PIL import Image
 from io import BytesIO
+
+
+def get_image_data(image_name:str)->bytes:
+    '''Gets image data as bytes for ready to post'''
+    with open(f'{image_name}.png','rb') as image_file:
+        image_data = image_file.read()
+    return base64.b64encode(image_data).decode() # for to fit in json.dumps()
+
 
 def compress_image(image_name:str)->None:
     '''Takes image name as an input find it and compress it'''
@@ -16,14 +25,19 @@ def compress_image(image_name:str)->None:
     img.save(f'{image_name}.png', 'PNG')
 
 
-
-def post_image(image_name:str,  image:str, url:str)-> None:
+def post_image(frame:int,image_name:str,  image:bytes, url:str)-> None:
     '''Sends image and device data to server'''
     device_name = socket.gethostname()
-    data = f'"device_name": "{device_name}", "image_name": "{image_name}", "image": "{image}"'
-    data = '{'+data+'}'
+    data = {
+                "frame":frame,
+                "device_name": device_name,
+                "image_name": image_name,
+                "image": image              
+                                            }
+    data = json.dumps(data)
     response = requests.post(url,  data=data)
     return response
+
 
 def run_camera(file_name:str)-> None:
     '''Runs camera and take shot every 2 sec and overwrite it on same image'''
@@ -31,10 +45,17 @@ def run_camera(file_name:str)-> None:
     camera.rotation=270 #For to fix image output
     camera.start_preview()
     sleep(2)
+    frame_count=0
     for filename in camera.capture_continuous(f'{file_name}.jpg'):
-        compress_image(file_name)
+        frame_count += 1
+        compress_image(file_name) # compressing image file
+        compressed_image = get_image_data(file_name)
         try:
-            post_image(image_name=filename,image='image',url='http://192.168.88.132:8000/images/')
+            post_image(
+                        frame=frame_count,
+                        image_name=filename,
+                        image=compressed_image,
+                        url='http://192.168.88.132:8000/images/')
         except socket.error as error: # For to prevent crush when connection lost or when pi can not connect
             print('Server connection error ')
         sleep(2) # wait 2 sec
